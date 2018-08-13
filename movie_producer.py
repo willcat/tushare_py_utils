015bac17e6a2eca9a9718ca0774506d8a6c24ae1
willcat <yunxiuhe@catchingmedia.com>
2018-08-13 16:34:35 +0800

    change producer's batch_size

--- movie_producer.py
+++ movie_producer.py
@@ -3,7 +3,7 @@ from kafka import KafkaProducer
 import json
 import time,datetime
 df = ts.realtime_boxoffice()
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
+producer = KafkaProducer(bootstrap_servers='localhost:9092',batch_size=100)
 
 def parseArray(value):
     arr = {}
@@ -15,9 +15,8 @@ def parseArray(value):
     arr['sumBoxOffice'] = value[5]
     arr['timeStamp'] = int(time.mktime(time.strptime(value[6],"%Y-%m-%d %H:%M:%S")))
     return arr
-i=0
+
 for idx in df.index:
     message = df.loc[idx].values
     dics = parseArray(df.loc[idx].values)
     producer.send('foobar', bytes(json.dumps(dics),encoding='utf-8'))
-    producer.flush()
