088adc8cc8d7b7380511f4698eceb365fd453141
willcat <yunxiuhe@catchingmedia.com>
2018-08-13 16:20:19 +0800

    支持字典数据发送

--- movie_producer.py
+++ movie_producer.py
@@ -1,9 +1,23 @@
 import tushare as ts
 from kafka import KafkaProducer
 import json
+import time,datetime
 df = ts.realtime_boxoffice()
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+def parseArray(value):
+    arr = {}
+    arr['boxOffice'] = value[0]
+    arr['rank'] = value[1]
+    arr['movieName'] = value[2]
+    arr['boxPer'] = value[3]
+    arr['movieDay'] = value[4]
+    arr['sumBoxOffice'] = value[5]
+    arr['timeStamp'] = int(time.mktime(time.strptime(value[6],"%Y-%m-%d %H:%M:%S")))
+    return arr
+i=0
 for idx in df.index:
-    message = df.loc[idx].values[0:-1].tolist()
-    producer.send('foobar', bytes(json.dumps(message),encoding='utf-8'))
-    print(message)
+    message = df.loc[idx].values
+    dics = parseArray(df.loc[idx].values)
+    producer.send('foobar', bytes(json.dumps(dics),encoding='utf-8'))
+    producer.flush()
